47dc3a527a852369b65f658afde7e9d95e96a451
Sajeeb <sajeeb2186@gmail.com>
2025-06-10 23:48:58 +0600

    form added

--- src/app/SignIn/page.jsx
+++ src/app/SignIn/page.jsx
@@ -1,23 +1,27 @@
-import Image from 'next/image'
-import React from 'react'
+import Image from "next/image";
+import React from "react";
 
 export default function page() {
   return (
-    <div> 
-
-      <div className='grid grid-cols-2 gap-12'>
-
+    <div>
+      <div className="grid grid-cols-2 gap-12">
         <div>
-          <Image src="/assets/images/login/login.svg" height="540" width="540"  alt='login image'/>
+          <Image
+            src="/assets/images/login/login.svg"
+            height="540"
+            width="540"
+            alt="login image"
+          />
         </div>
 
         <div>
-          form
+          <fieldset className="fieldset">
+            <legend className="fieldset-legend">What is your name?</legend>
+            <input type="text" className="input" placeholder="Type here" />
+            <p className="label">Optional</p>
+          </fieldset>
         </div>
-        
       </div>
-      
-      
     </div>
-  )
+  );
 }
