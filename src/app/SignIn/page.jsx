a1cfc270af9cb8a592b182e48a0162b8c7b4b148
Sajeeb <sajeeb2186@gmail.com>
2025-06-17 12:23:23 +0600

    sign in and sign up page setup done

--- src/app/SignIn/page.jsx
+++ src/app/SignIn/page.jsx
@@ -1,10 +1,23 @@
+
+"use client"
 import Image from "next/image";
 import React from "react";
+import { FcGoogle} from "react-icons/fc";
+import { FaGithub } from "react-icons/fa";
+import Link from "next/link";
 
 export default function page() {
+
+
+  const handlaLogin=async()=>{
+
+  }
+
+
+
   return (
-    <div>
-      <div className="grid grid-cols-2 gap-12">
+    <div className="container mx-auto gap-12 py-24">
+      <div className="grid grid-cols-2 gap-12 items-center">
         <div>
           <Image
             src="/assets/images/login/login.svg"
@@ -14,15 +27,36 @@ export default function page() {
           />
         </div>
 
-        <div>
-          <fieldset className="fieldset">
-            
-            <legend className="fieldset-legend">Email</legend>
-            <input type="text" name="email" className="input" placeholder="Your Email" />
-            <legend className="fieldset-legend">Password</legend>
-            <input type="text" name="password" className="input" placeholder="Your Password" /> 
-            
-          </fieldset>
+        <div className="border-2 p-12">
+
+          <h6 className="text-3xl font-semibold text-center mb-12">Sign In</h6>
+
+          <form onSubmit={handlaLogin} action="">
+            <label htmlFor="email"> Email:</label>
+          <input type="text" name="email" placeholder="Your Email" className=" mt-3 input input-bordered w-full " />
+          <br /> <br />
+          <label htmlFor="password"> Password:</label>
+          <input type="text" name="password" placeholder="Yours Password" className=" mt-3 input input-bordered w-full " />
+          <br /> <br />
+
+          <button type="submit" className=" w-full btn btn-primary mt-12">Sign In</button>
+
+          </form>
+
+          
+          <div>
+            <h6 className=" text-center my-4">or sign in with</h6>
+            <div className=" flex  items-center justify-center gap-2">
+              <button className="btn"> <FcGoogle/></button>
+              <button className="btn"> <FaGithub/></button>
+            </div>
+
+            <h6 className=" text-center my-4">Don't have accout? <Link className="text-primary font-semibold" href={'/SignUp'}>SignUp</Link></h6>
+
+          </div>
+
+          
+         
         </div>
       </div>
     </div>
--- src/app/SignUp/page.jsx
+++ src/app/SignUp/page.jsx
@@ -1,7 +1,65 @@
+"use client"
+import Image from 'next/image'
+import Link from 'next/link'
 import React from 'react'
+import { FaGithub } from 'react-icons/fa'
+import { FcGoogle } from 'react-icons/fc'
 
 export default function page() {
+
+   const handlaLogin=async()=>{
+
+  }
+
+
+
   return (
-    <div> sign up page</div>
+    <div className="container mx-auto gap-12 py-24">
+      <div className="grid grid-cols-2 gap-12 items-center">
+        <div>
+          <Image
+            src="/assets/images/login/login.svg"
+            height="540"
+            width="540"
+            alt="login image"
+          />
+        </div>
+
+        <div className="border-2 p-12">
+
+          <h6 className="text-3xl font-semibold text-center mb-12">Sign Up</h6>
+
+          <form onSubmit={handlaLogin} action="">
+             <label htmlFor="name"> Name:</label>
+          <input type="text" name="name" placeholder="Your Name" className=" mt-3 input input-bordered w-full " />
+          <br /><br />
+            <label htmlFor="email"> Email:</label>
+          <input type="text" name="email" placeholder="Your Email" className=" mt-3 input input-bordered w-full " />
+          <br /> <br />
+          <label htmlFor="password"> Password:</label>
+          <input type="text" name="password" placeholder="Yours Password" className=" mt-3 input input-bordered w-full " />
+          <br /> <br />
+
+          <button type="submit" className=" w-full btn btn-primary mt-12">Sign Up</button>
+
+          </form>
+
+          
+          <div>
+            <h6 className=" text-center my-4">or sign in with</h6>
+            <div className=" flex  items-center justify-center gap-2">
+              <button className="btn"> <FcGoogle/></button>
+              <button className="btn"> <FaGithub/></button>
+            </div>
+
+            <h6 className=" text-center my-4">Already have accout? <Link className="text-primary font-semibold" href={'/SignIn'}>SignIn</Link></h6>
+
+          </div>
+
+          
+         
+        </div>
+      </div>
+    </div>
   )
 }
