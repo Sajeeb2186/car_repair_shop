6b64059665629c5e95447aba44cefb80221121fb
Sajeeb <sajeeb2186@gmail.com>
2025-06-27 18:47:50 +0600

    checkout page

--- src/app/checkout/[id]/page.jsx
+++ src/app/checkout/[id]/page.jsx
@@ -1,9 +1,15 @@
+import { getServiceDetails } from '@/services/getServices';
 import Image from 'next/image'
 import React from 'react'
 
-export default function page() {
+export default async  function page({params}) {
+
+  // const details =await getServiceDetails(params.id)
+  //     const {_id, title, description, img, price, facility} = details;
+
+
   return (
-     <div>
+     <div className='mx-40'>
               <div className="relative  h-72">
                 <Image
                   className="absolute h-72 w-full left-0 top-0 object-cover"
@@ -15,9 +21,83 @@ export default function page() {
                 />
                 <div className="absolute h-full left-0 top-0 flex items-center justify-center bg-gradient-to-r from-[#151515] to-[rgba(21, 21, 21, 0)] ">
                   <h1 className="text-white text-3xl font-bold flex justify-center items-center ml-8">
-                     Details 
+                     checkout of 
                   </h1>
                 </div>
+              </div>
+
+              <div className=' my-12 bg-slate-300 p-12'>
+                <form >
+          <div className="grid grid-cols-1 md:grid-cols-2 gap-6">
+            <div className="form-control">
+              <label className="label">
+                <span className="label-text">Name</span>
+              </label>
+              <input  type="text" name="name" className="input input-bordered" />
+            </div>
+            <div className="form-control">
+              <label className="label">
+                <span className="label-text">Date</span>
+              </label>
+              <input  type="date" name="date" className="input input-bordered" />
+            </div>
+            <div className="form-control">
+              <label className="label">
+                <span className="label-text">Email</span>
+              </label>
+              <input
+             
+                type="text"
+                name="email"
+                placeholder="email"
+                className="input input-bordered"
+              />
+            </div>
+            <div className="form-control">
+              <label className="label">
+                <span className="label-text">Due amount</span>
+              </label>
+              <input
+              
+              readOnly
+                type="text"
+                name="price"
+                className="input input-bordered"
+              />
+            </div>
+            <div className="form-control">
+              <label className="label">
+                <span className="label-text">Phone</span>
+              </label>
+              <input
+              required
+                type="text"
+                name="phone"
+                placeholder="Your Phone"
+                className="input input-bordered"
+              />
+            </div>
+            <div className="form-control">
+              <label className="label">
+                <span className="label-text">Present Address</span>
+              </label>
+              <input
+                type="text"
+                name="address"
+                placeholder="Your Address"
+                className="input input-bordered"
+              />
+            </div>
+          </div>
+          <div className="form-control mt-6">
+            <input
+              className="btn btn-primary btn-block"
+              type="submit"
+              value="Order Confirm"
+            />
+          </div>
+        </form>
+                
               </div>
     
 
--- src/components/shared/Footer.jsx
+++ src/components/shared/Footer.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 
 export default function Footer() {
   return (
-    <footer className="footer sm:footer-horizontal bg-base-200 text-base-content p-10 ">
+    <footer className="footer sm:footer-horizontal bg-base-200 text-base-content p-10 mx-40 ">
   <aside>
     <svg
       width="50"
